f37123fc2f96f68a80cd1f8b9d1bbe88a4479b97
Fernando Vinicius <fernando@realworld.com.br>
2022-07-18 19:50:29 -0300

    Update pset 72.py

---
+++ Exercises/pset 72.py
@@ -0,0 +1,8 @@
+#Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+numbers = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+
+while True:
+  user_name = int(input('Digite um número de 0 a 20: '))
+  if user_name >= 0 and user_name <= 20:
+      break
