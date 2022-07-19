f6e3ddbdeb997d36431313f276e2847c9c552317
Fernando Vinicius <fernando@realworld.com.br>
2022-07-19 14:57:02 -0300

    Update pset 074.py

--- Exercises/pset 074.py
+++ Exercises/pset 074.py
@@ -1,9 +1,11 @@
-#Exercício Python 074: Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+# Exercício Python 074: Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
 
 from random import randint
 
-numbers = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+numbers = (randint(1, 10), randint(1, 10), randint(
+    1, 10), randint(1, 10), randint(1, 10))
 print('Os valores sorteados foram: ', end='')
 for n in numbers:
-  print(f'{n}', end=' ')
-
+    print(f'{n}', end=' ')
+print(f'\nO maior valor foi {max(numbers)}')
+print(f'O menor valor foi {min(numbers)}')
